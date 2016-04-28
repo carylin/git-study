09588048825bf5986a54a09023a195c4e1989923
LSh <LSh@DESKTOP-DGMPVC2>
2016-04-28 23:43:46 +0800

    aa

--- git-project/src/com/cary/Main.java
+++ git-project/src/com/cary/Main.java
@@ -3,7 +3,8 @@ package com.cary;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world 5!");
+		System.out.println("hello world 7!");
+		int i = 10;
 	}
 
 }
